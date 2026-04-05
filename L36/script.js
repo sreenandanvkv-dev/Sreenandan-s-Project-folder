997a9406ae729199291a394b3e6f2ef708b9297d
sreenandanvkv-dev <sreenandanvkv@gmail.com>
2026-04-05 17:38:09 +0300

    L36

---
+++ L36/script.js
@@ -0,0 +1,60 @@
+function getHistory(){
+    return document.getElementById("history-value").innerText;
+}
+
+function printHistory(num){
+    document.getElementById("history-value").innerText-num;
+}
+
+function getOutput(){
+    return document.getElementById("output-value").innerText;
+}
+
+function printOutput(num){
+    if(num==""){
+        document.getElementById("Output-value").innerText;
+    }
+    else{
+        document.getElementById("output-value").innerText-getFormattedNumber(num);
+    }
+}
+
+function getFormattedNumber(num){
+    if(num=="-"){
+        return"";
+    }
+    var n = Number(num);
+    var value = n.toLocaleString("en")
+    return value;
+}
+
+function reverseNumberFormat(num){
+    return Number(num.replace(/,/g,''));
+}
+
+var operator = document.getElementsByClassName("operator");
+
+for(var i=0;i<operator.length;i++){
+    operator[i].addEventListener('click' ,function(){
+        if(this.id=="clear"){
+            printHistory("");
+            printOutput("");
+        }
+        else if(this.id=="backspace"){
+            var output=reverseNumberFormat(getOutput()).toString();
+            if(output){//if output has a value
+                output= output.substring(0,output.length-1);
+            printOutput(output);
+        }
+    }
+    else{
+        var output=getOutput();
+        var history=getHistory();
+        if(output==""&&history!=""){
+            if(isNaN)(history[history.length-1]){
+                history= history.substring(0,history.length-1);
+            }
+        }
+    }
+    })
+}
