2e9070b958d525c6e89f1660212033e43f60aa6d
sreenandanvkv-dev <sreenandanvkv@gmail.com>
2026-04-09 16:31:10 +0300

    L37

---
+++ L37/L37 Print Statement and Comments.py
@@ -0,0 +1,12 @@
+#display greeting message
+print("hello there")
+
+print()
+
+#display any characters
+print("hi there I am #175 Sreenandan.")
+
+print()
+
+#display calculation
+print("3+5 =", 3+5)
---
+++ L37/L37 Welcome to The World of Programming.py
@@ -0,0 +1,14 @@
+#Print a Sentence
+print("welcome to the world of programming!")
+
+#print a number
+print(4)
+
+#Next ling after print
+print("Hello Python \n")
+
+import keyword as kw
+
+#print all the keywords present in Python
+print("Keywords in Python are...")
+print(kw.kwlist)
