d49e661a49f2063435476faf454d5fe649b3f6c7
sreenandanvkv-dev <sreenandanvkv@gmail.com>
2026-05-07 16:34:17 +0300

    L41

---
+++ L41/L41 introduce urself.py
@@ -0,0 +1,5 @@
+def intro(name): #declaring function with parameter
+    print("Hello, Good morning! i am", name)
+
+user_name = input("Enter your name: ")
+intro(user_name) #calling function and passing argument
